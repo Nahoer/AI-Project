7233ef6985195e3382ff9c7313bea51db243483f
Nahoer <56316648+Nahoer@users.noreply.github.com>
2023-01-06 10:31:24 +0100

    update config yaml

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 if __name__ == '__main__':
     # clone YOLOv5 repository
     #os.system("!git clone https://github.com/ultralytics/yolov5")  # change directory
-    #os.system("pip install - qr requirements.txt  # install dependencies (ignore errors)#start training")
+    #os.system("pip install -qr requirements.txt  # install dependencies (ignore errors)#start training")
     #os.environ["GIT_PYTHON_REFRESH"] = "quiet"
-    os.system("yolov5/python train.py --img 640 --batch 2 --epochs 1 --data 'LicensePlate_dataset.yaml' --cfg. /models/yolov5s.yaml --weights '' --name yolov5s_results --cache")
+    os.system("python train.py --img 640 --batch 2 --epochs 1 --data 'LicensePlate_dataset.yaml' --weights yolov5s.pt --name yolov5s_results")
     os.system("load_ext tensorboard")
     os.system("tensorboard --logdir runs")
